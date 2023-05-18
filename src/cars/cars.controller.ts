834ae7abd5b77fc353310d66140c2dfeb1112980
EmmanuelJCA <emmanuelcanate@gmail.com>
2023-05-17 22:59:12 -0400

    Post method and validations

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post} from '@nestjs/common';
 import { CarsService } from './cars.service';
 import { CreateCarDto } from './dto/create-car.dto';
+import { UpdateCarDto } from './dto/update-car.dto';
 
 @Controller('cars')
 export class CarsController {
@@ -21,13 +22,13 @@ export class CarsController {
 
   @Post()
   createCar( @Body() createCarDto: CreateCarDto ) {
-    return createCarDto;
+    return this.carsService.create(createCarDto);
   }
 
   @Patch(':id')
   updateCar( 
-    @Param('id', ParseIntPipe) id: number, 
-    @Body() body: any ) 
+    @Param('id', ParseUUIDPipe) id: string, 
+    @Body() updateCarDto: UpdateCarDto ) 
   {
     return { id, body };
   }
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { v4 as uuid } from 'uuid';
 import { Car } from './interfaces/car.interface';
+import { CreateCarDto } from './dto/create-car.dto';
+import { UpdateCarDto } from './dto/update-car.dto';
 
 @Injectable()
 export class CarsService {
@@ -31,5 +33,18 @@ export class CarsService {
     const car = this.cars.find( car => car.id == id);
     if( !car ) throw new NotFoundException(`Car with id '${ id }' not found`);
     return car;
+  }
+
+  create( createCarDto: CreateCarDto) {
+    const car: Car = {
+      id: uuid(),
+      ...createCarDto
+    };
+     this.cars = [...this.cars, car];
+     return car
+  }
+
+  update( id: string, updateCarDto: UpdateCarDto ) {
+
   }
 }
